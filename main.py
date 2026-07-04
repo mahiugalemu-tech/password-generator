5bfcaf6d30eadf72b4d439d4c843cf76cd221fac
mahiugalemu-tech <mahiugale.mu@gmail.com>
2026-07-05 00:24:03 +0530

    Add password generator
    Created the first version of the project.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import random
+import string
 
+print("=== Password Generator ===")
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ''.join(random.choice(characters) for _ in range(length))
+
+print("Generated Password:")
+print(password)
